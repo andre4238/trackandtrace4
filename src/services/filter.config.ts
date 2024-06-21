85768c64ac91badd6a739c1bb140556ebc3f2a34
andre4238 <andre.lobach@icloud.com>
2024-06-22 01:11:51 +0200

    added details box

--- src/services/filter.config.ts
+++ src/services/filter.config.ts
@@ -4,6 +4,7 @@ export const EMAIL_FILTERS: { [email: string]: string[] } = {
     'li@gmail.com': ['Launch Italy', 'Launch Italien', 'LAUNCH Italy', 'LAUNCH ITALY', 'Launch italy','launch italy', 'launch Italy'],
     'dts@mail.com': ['dts', 'DTS'],
     'team-was@was-info.com': ['was', 'WAS'],
+
     // Weitere E-Mail-Adressen und Filter hier hinzufügen
   };
-  
+
