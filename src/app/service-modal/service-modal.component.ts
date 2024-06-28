2fdf64a0f5b30b52deffa79e6b7a28f7c82508d8
andre4238 <andre.lobach@icloud.com>
2024-06-28 22:16:18 +0200

    service modal test

--- src/app/service-modal/service-modal.component.ts
+++ src/app/service-modal/service-modal.component.ts
@@ -8,11 +8,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class ServiceModalComponent {
   @Input() task: any;
   @Input() trackingNumber: string = '';
-
   @Input() fedexStatus: any;
   @Output() closeModal = new EventEmitter<void>();
 
   close() {
     this.closeModal.emit();
+  }
+
+  submitRequest() {
+    // Hier können Sie den Logik zur Verarbeitung des Service Requests hinzufügen
+    console.log('Service Request Submitted');
+    this.close();
   }
 }
