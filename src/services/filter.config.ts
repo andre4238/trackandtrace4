c03540f1e1f09e532902286b12c20e2ce455cab5
johnnyguma <Johnnyguma17@gmail.com>
2025-04-15 12:37:14 +0200

    added customer to DB

--- src/services/filter.config.ts
+++ src/services/filter.config.ts
@@ -8,7 +8,7 @@ export const EMAIL_FILTERS: { [email: string]: string[] } = {
     'vehidiag@gmail.com': ['Vehi', 'Vehi Diag', 'vehi diag'],
     'ddc@test.com': ['ddc', 'DDC'],
     'a.lobach@launch-europe.de': ['CZ','SK','HU','IHR', 'ihr','Ihr','Univer','UNIVER', 'LKQ','Inter Cars','IC','INTER CARS', 'MTA','Mta','MtaPlus','mtaplus', 'WM CZ','SAG','SAG CZ','Slavicek', 'Auto Slavicek','AD Partner','J+M','j+m', 'IAG','IAG CZ','Wrobel','RSP', 'RSP Trading','Spitec','SpiTec','IC SK','Peterson Technik', 'Jantolak','Innovation center','Innovation Center','WM SK','Baranek','Autotechna Baranek', 'Homola', 'homola','Homola SK'],
-    'info@autodiagnostika.sk': ['Jantolak', 'jantolak', 'Jantolak SK'],
+    'info@autodiagnostika.sk': ['Jantolak', 'jantolak', 'Jantolak SK', 'Ing. Jantolak','Jantolák'],
   // Weitere E-Mail-Adressen und Filter hier hinzufügen
   };
 
