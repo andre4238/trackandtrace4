a0523f3c71558bf96f3ef21568856ea3fc5da554
johnnyguma <Johnnyguma17@gmail.com>
2025-04-15 12:36:24 +0200

    Changed Login Formular to Fluent

--- src/services/filter.config.ts
+++ src/services/filter.config.ts
@@ -7,7 +7,8 @@ export const EMAIL_FILTERS: { [email: string]: string[] } = {
     'coler@coler.de': ['coler', 'EKB', 'Coler'],
     'vehidiag@gmail.com': ['Vehi', 'Vehi Diag', 'vehi diag'],
     'ddc@test.com': ['ddc', 'DDC'],
-
-    // Weitere E-Mail-Adressen und Filter hier hinzufügen
+    'a.lobach@launch-europe.de': ['CZ','SK','HU','IHR', 'ihr','Ihr','Univer','UNIVER', 'LKQ','Inter Cars','IC','INTER CARS', 'MTA','Mta','MtaPlus','mtaplus', 'WM CZ','SAG','SAG CZ','Slavicek', 'Auto Slavicek','AD Partner','J+M','j+m', 'IAG','IAG CZ','Wrobel','RSP', 'RSP Trading','Spitec','SpiTec','IC SK','Peterson Technik', 'Jantolak','Innovation center','Innovation Center','WM SK','Baranek','Autotechna Baranek', 'Homola', 'homola','Homola SK'],
+    'info@autodiagnostika.sk': ['Jantolak', 'jantolak', 'Jantolak SK'],
+  // Weitere E-Mail-Adressen und Filter hier hinzufügen
   };
 
